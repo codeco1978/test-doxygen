24438a2cc09b37f0f6976e2a4ac2a8ba203f403e
Ruben Mendez <ruben.mendez@zitrogames.com>
2023-10-16 14:38:42 +0200

    Añadido archivo para tests

---
+++ test.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file test.cpp
+ * @version 1.0
+ * @date 16/10/2023
+ * @author Ruben Mendez Hernandez
+ * @title Hola Mundo
+ * @code
+    int main() {
+        cout << "Hello World!";
+    }
+ * @endcode
+*/
+
+// Your First C++ Program
+
+#include <iostream>
+
+/**
+ * @brief main solo saca "Hello World!" por pantalla
+*/
+
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
